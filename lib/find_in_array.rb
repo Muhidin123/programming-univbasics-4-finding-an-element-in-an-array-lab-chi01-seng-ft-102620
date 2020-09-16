645a6fb9a3c35f58c21abbd65267d9a643a36e33
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-16 19:53:26 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,3 @@
-def find_element_index(array, value_to_find)
-  if array.include?(value_to_find)
-    array.index(value_to_find)
-  else
-    nil
-  end
+def find(arr, element)
+  arr.include?(element) ; puts arr.index(element) ; nil
 end
